3b1ac3eb65a5a884e81c7e1e72757c78096935de
Minh Tran <minhtd@enguys.com>
2016-05-28 17:43:02 +0700

    Sort input

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -25,46 +25,68 @@ const colorInfo = (color) => {
   return sortedColors[0];
 };
 
+const sortColors = (colors) => {
+  let black = chroma('#000000');
+  colors.sort((colorA, colorB) => {
+    let colorADistance = distance(chroma(colorA).lab(), black.lab());
+    let colorBDistance = distance(chroma(colorB).lab(), black.lab());
+    return colorADistance - colorBDistance;
+  });
+  return colors;
+};
+
 const inputEl = document.getElementById('input');
 const outputEl = document.getElementById('output');
 const sassEl = document.getElementById('sass');
+const sortSubmitEl = document.getElementById('sort-submit');
+
+const getValue = (el) => {
+  let input = el.value.trim();
+  let colors = input.split('\n');
+  return colors.map(color => color.trim());
+};
+
+const setValue = (el, values) => {
+  el.value = values.join('\n');
+};
 
 let timer;
-inputEl.addEventListener('keydown', (e) => {
+const handleChange = () => {
   if (timer) {
     clearTimeout(timer);
   }
-  timer = setTimeout(() => {
-    handleChange(e.target.value, sassEl.checked);
+  setTimeout(() => {
+    let colors = getValue(inputEl);
+    let sass = sassEl.checked;
+    let values = colors.map(color => {
+      let info = colorInfo(color);
+      if (!info) {
+        return 'invalid';
+      }
+      if (!sass) {
+        return info.name;
+      }
+      let name = info.name
+        .replace(/[^a-zA-Z0-9 ]/g, '')
+        .replace(/ /g, '-')
+        .toLowerCase();
+      return `$${name}: ${info.hex};`;
+    });
+    setValue(outputEl, values);
   }, 200);
-});
-
-sassEl.addEventListener('change', (e) => {
-  if (timer) {
-    clearTimeout(timer);
-  }
-  timer = setTimeout(() => {
-    handleChange(inputEl.value, e.target.checked);
-  }, 200);
-});
-
-const handleChange = (input = '', sass = false) => {
-  input = input.trim();
-  let colors = input.split('\n');
-  let names = colors.map(color => {
-    color = color.trim();
-    let info = colorInfo(color.trim());
-    if (!info) {
-      return 'invalid';
-    }
-    if (!sass) {
-      return info.name;
-    }
-    let name = info.name
-      .replace(/[^a-zA-Z0-9 ]/g, '')
-      .replace(/ /g, '-')
-      .toLowerCase();
-    return `$${name}: ${info.hex};`;
-  });
-  outputEl.value = names.join('\n');
 };
+
+inputEl.addEventListener('keydown', () => {
+  handleChange();
+});
+
+sassEl.addEventListener('change', () => {
+  handleChange();
+});
+
+sortSubmitEl.addEventListener('click', () => {
+  let colors = getValue(inputEl);
+  colors = sortColors(colors);
+  setValue(inputEl, colors);
+  handleChange();
+});
